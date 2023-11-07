a84a0606360903cc93abbfeea399880a8637f643
Michael Shitrit <mshitrit@redhat.com>
2023-11-07 17:31:46 +0200

    Adding a default template label
    Signed-off-by: Michael Shitrit <mshitrit@redhat.com>

--- pkg/labels/labels.go
+++ pkg/labels/labels.go
@@ -7,4 +7,6 @@ const (
 	MasterRole = "node-role.kubernetes.io/master"
 	// ControlPlaneRole is the new role label of control plane nodes
 	ControlPlaneRole = "node-role.kubernetes.io/control-plane"
+	// DefaultTemplate is a label that would be set in case a remediator may have several remediation templates in order to signal to the UI which one is the default
+	DefaultTemplate = "remediation.medik8s.io/default-template"
 )
