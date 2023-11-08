265e303429e839a568ff58e721cacd88a028aa90
Michael Shitrit <mshitrit@redhat.com>
2023-11-08 11:26:08 +0200

    update label description
    Signed-off-by: Michael Shitrit <mshitrit@redhat.com>

--- pkg/labels/labels.go
+++ pkg/labels/labels.go
@@ -7,6 +7,6 @@ const (
 	MasterRole = "node-role.kubernetes.io/master"
 	// ControlPlaneRole is the new role label of control plane nodes
 	ControlPlaneRole = "node-role.kubernetes.io/control-plane"
-	// DefaultTemplate is a label that would be set in case a remediator may have several remediation templates in order to signal to the UI which one is the default
+	// DefaultTemplate label indicates to third party tools (e.g. UI) the default remediation template in case several exists.
 	DefaultTemplate = "remediation.medik8s.io/default-template"
 )
