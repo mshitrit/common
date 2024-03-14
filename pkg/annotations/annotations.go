f7230776b17ced1cb7076eec40ec043bc5a23200
Michael Shitrit <mshitrit@redhat.com>
2024-03-14 15:42:35 +0200

    Add MultipleTemplatesSupportedAnnotation
    Signed-off-by: Michael Shitrit <mshitrit@redhat.com>

--- pkg/annotations/annotations.go
+++ pkg/annotations/annotations.go
@@ -3,4 +3,7 @@ package annotations
 const (
 	// NhcTimeOut is the annotation set by NHC to signal the operator that it surpassed its timeout and shall stop its remediation
 	NhcTimedOut = "remediation.medik8s.io/nhc-timed-out"
+
+	// MultipleTemplatesSupportedAnnotation is an annotation that indicates whether multiple templates of the same kind are supported by the template's remediator
+	MultipleTemplatesSupportedAnnotation = "remediation.medik8s.io/multiple-templates-support"
 )
